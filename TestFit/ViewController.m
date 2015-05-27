74f31bbd349ba57c0dedd538b1a1ef22b99ef7e5
iceboxi <iceboxi@gmail.com>
2015-05-27 17:41:04 +0800

    sample Label view

--- TestFit/ViewController.m
+++ TestFit/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSString *string;
+}
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nextLabel;
 
 @end
 
@@ -17,6 +21,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    string = @"超級長長長長長長的字串串串串串串串串串串翅";
+    self.testLabel.text = string;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    CGRect frame = self.testLabel.frame;
+    CGSize size = [string sizeWithFont:self.testLabel.font constrainedToSize:CGSizeMake(frame.size.width, frame.size.height) lineBreakMode:UILineBreakModeTailTruncation];
+    
+    self.testLabel.frame = CGRectMake(frame.origin.x, frame.origin.y, size.width, size.height);
+    
+    CGRect frame2 = self.nextLabel.frame;
+    self.nextLabel.frame = CGRectMake(frame.origin.x+size.width, frame2.origin.y, frame2.size.width, frame2.size.height);
 }
 
 - (void)didReceiveMemoryWarning {
