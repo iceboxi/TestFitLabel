54ceecd09f1e1e283fc57262c6ff978a1d2d914d
iceboxi <iceboxi@gmail.com>
2015-05-27 15:03:23 +0800

    Initial Commit

---
+++ TestFit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFit
+//
+//  Created by iceboxi on 2015/5/27.
+//  Copyright (c) 2015年 iceboxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFit
+//
+//  Created by iceboxi on 2015/5/27.
+//  Copyright (c) 2015年 iceboxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
